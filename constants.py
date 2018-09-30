1ec029a7148ad2f8a706b76c1099b1519953a439
Bartleby <bartleby2718@gmail.com>
2018-09-30 10:47:06 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,15 +1,23 @@
 DECK_PER_PILE = 9
 CARD_PER_DECK = 3
 RANKS = ('Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King')
+HIGHEST_VALUE = 13
 RED_SUITS = ('Hearts', 'Diamonds')
 BLACK_SUITS = ('Spades', 'Clubs')
+PLAYER_RED = 'Player Red'
+PLAYER_BLACK = 'Player Black'
+REQUIRED_WIN = 3
+TIME_LIMIT_FOR_ACTION = 7
+MAX_DIE = 2
+MAX_DONE = 1
+MAX_DRAW = 1
 
 
 class Action(object):
-    DARE = 'dare'
-    DIE = 'die'
-    DONE = 'done'
-    DRAW = 'draw'
+    DARE = 'Dare'
+    DIE = 'Die'
+    DONE = 'Done'
+    DRAW = 'Draw'
 
 
 class DeckState(object):
@@ -20,15 +28,16 @@ class DeckState(object):
 
 class DuelResult(object):
     DRAWN = 'Drawn'
-    GAME = 'Finished'
-    DONE = 'Done'
+    FINISHED = 'Finished'
+    DIED = 'Died'
     ABORTED_BY_DONE = 'Aborted by done'
     ABORTED_BY_FORFEIT = 'Aborted by forfeit'
+    FORFEITED_BY_WRONG_DONE = 'Forfeited by wrong done'
 
 
 class GameResult(object):
     DRAWN = 'Drawn'
     GAME = 'Finished'
     DONE = 'Done'
-    FORFEITED_BY_DONE = 'Forfeited by done'
-    FORFEITED_BY_DRAW = 'Forfeited by draw'
+    FORFEITED_BY_WRONG_DONE = 'Forfeited by wrong done'
+    FORFEITED_BY_WRONG_DRAW = 'Forfeited by wrong draw'
