40a8860464c2507879027faf4182d5c8efeac997
Jihoon Park <bartleby2718@gmail.com>
2018-12-22 15:40:35 +0900

    Add moving average to time graph close #135

--- visualization.py
+++ visualization.py
@@ -2,6 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
+
+def moving_average(array_like, window):
+    cumulative_sum = np.cumsum(np.insert(array_like, 0, 0))
+    return (cumulative_sum[window:] - cumulative_sum[:-window]) / float(window)
+
+
 current_directory = os.path.dirname(os.path.realpath(__file__))
 csv_file_path = os.path.join(current_directory, 'sample.csv')
 data = np.genfromtxt(csv_file_path, delimiter=',',
@@ -18,9 +24,13 @@ plt.legend()
 plt.show()
 
 # TODO: epoch vs time
-plt.plot(data['epoch'], data['time'], color='r', label='sample')
+window = 20
+moving_average_time = moving_average(data['time'], window)
+plt.plot(data['epoch'], data['time'], color='r', label='time elapsed')
+plt.plot(data['epoch'][window - 1:], moving_average_time, color='b',
+         label='{}-epoch mov. avg.'.format(window))
 plt.xlabel('epoch')
-plt.ylabel('time')
+plt.ylabel('time (seconds)')
 plt.title('epoch vs time')
 plt.legend()
 plt.show()
